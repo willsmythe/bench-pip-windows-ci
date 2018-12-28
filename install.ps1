c41ec921e8ef0ca182665d4bb2c94546aa72ec72
Nathaniel J. Smith <njs@pobox.com>
2018-12-27 23:25:03 -0800

    install correct pip into the venv

--- install.ps1
+++ install.ps1
@@ -5,6 +5,7 @@ myenv\Scripts\activate
 
 python --version
 python -c "import struct; print(struct.calcsize('P') * 8, 'bits')"
+pip install pip==$env:PYTHON_PIP
 pip --version
 
 Measure-Command {pip install -r requirements.txt}
