5dab891eaf1057cc21d91fc18c1b199beb164276
Nathaniel J. Smith <njs@pobox.com>
2018-12-27 23:30:04 -0800

    pip version doesn't seem to matter, so use latest pip consistently

--- install.ps1
+++ install.ps1
@@ -2,10 +2,10 @@ Set-PSDebug -Trace 1
 
 python -m venv myenv
 myenv\Scripts\activate
+python -m pip install -U pip
 
 python --version
 python -c "import struct; print(struct.calcsize('P') * 8, 'bits')"
-pip install pip==$env:PYTHON_PIP
 pip --version
 
 Measure-Command {pip install -r requirements.txt}
