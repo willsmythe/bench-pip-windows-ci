f0985db3f517ebddc89a04ca34687bd4331fb0ec
Nathaniel J. Smith <njs@pobox.com>
2018-12-28 18:40:34 -0800

    Simplify further

--- install.ps1
+++ install.ps1
@@ -1,7 +1,14 @@
 Set-PSDebug -Trace 1
 
-python -m venv myenv
-myenv\Scripts\activate
+# Steve Dower suggested that using a venv might make things faster on Azure,
+# so we'll try it both ways.
+# https://twitter.com/zooba/status/1078548597195497472
+param([switch] $UseVenv)
+if ($UseVenv) {
+    python -m venv myenv
+    myenv\Scripts\activate
+}
+
 python -m pip install -U pip
 
 python --version
