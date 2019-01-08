4601f5a39c0fe505d14a0c74247cada0b2239f55
Nathaniel J. Smith <njs@pobox.com>
2019-01-08 00:24:43 -0800

    refreshenv?

--- ramdisk.ps1
+++ ramdisk.ps1
@@ -15,10 +15,9 @@ Set-PSDebug -Trace 1
 
 Invoke-WebRequest -Uri "http://www.ltr-data.se/files/imdiskinst.exe" -OutFile imdiskinst.exe
 .\imdiskinst.exe /fullsilent
+refreshenv
 
 imdisk
-
-refreshenv
 
 Add-MpPreference -ExclusionPath "r:\"
 
