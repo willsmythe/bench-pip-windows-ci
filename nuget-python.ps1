d9b94b45bd4ed36fd2b3bfd01057f06fd79a804b
Nathaniel J. Smith <njs@pobox.com>
2019-01-08 02:06:29 -0800

    more fun with nuget

---
+++ nuget-python.ps1
@@ -0,0 +1,28 @@
+Set-PSDebug -Trace 1
+
+mkdir scratch
+
+mkdir scratch\tmp
+mkdir scratch\cache
+$Env:TMPDIR = (Get-Item -Path scratch\tmp).FullName
+$Env:TMP = (Get-Item -Path scratch\tmp).FullName
+$Env:TEMP = (Get-Item -Path scratch\tmp).FullName
+$Env:PIP_CACHE_DIR = (Get-Item -Path scratch\cache).FullName
+
+# https://docs.python.org/3/using/windows.html#windows-nuget
+nuget.exe install python -ExcludeVersion -OutputDirectory scratch
+
+scratch\python\tools\python.exe -V
+
+ls scratch\python\tools
+ls scratch\python\tools\tools
+
+$Env:PATH = ((Get-Item -Path scratch\python\tools).FullName + $Env:PATH)
+python -V
+
+python -m ensurepip
+
+ls scratch\python\tools
+
+pip --version
+python -m pip install -U pip
