d5225ad7800b779571c02abeddba2cc31674166c
Nathaniel J. Smith <njs@pobox.com>
2019-01-08 02:40:26 -0800

    more stuff

--- zipped-python.ps1
+++ zipped-python.ps1
@@ -1,6 +1,7 @@
 Set-PSDebug -Trace 1
 
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.2/python-3.7.2-embed-amd64.zip" -OutFile python.zip
+# Use 3.7.1, because 3.7.2 is broken: https://bugs.python.org/issue35596
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.1/python-3.7.1-embed-amd64.zip" -OutFile python.zip
 
 # The official python-*.zip files from python.org have *two* copies of
 # vcruntime140.dll in them, for some reason. This annoys Expand-Archive, and
@@ -13,11 +14,9 @@ $Env:PATH = ((Get-Item -Path python-dir).FullName + ";" + $Env:PATH)
 
 python -V
 
-python -m ensurepip
-
-pip --version
+python -m pip --version
 
 python -m pip install -U pip
-pip --version
+python -m pip --version
 
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
