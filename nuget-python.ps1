51d2eb55644291b13d1247a6b00eacf61d0a0cf4
Nathaniel J. Smith <njs@pobox.com>
2019-01-08 18:00:20 -0800

    c'mon powershell

--- nuget-python.ps1
+++ nuget-python.ps1
@@ -11,7 +11,7 @@ $Env:TEMP = $scratch + "\tmp"
 mkdir scratch\pip-cache
 $Env:PIP_CACHE_DIR = $scratch + "\pip-cache"
 
-Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/python/3.7.2" -OutFile $scratch + "\python.nupkg"
+Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/python/3.7.2" -OutFile ($scratch + "\python.nupkg")
 nuget install $scratch + "\python.nupkg"
 
 # https://docs.python.org/3/using/windows.html#windows-nuget
