9e18d9ce22339976df1483faaa446ac5bb7db1b5
Will Smythe <wismythe@microsoft.com>
2019-01-14 12:58:48 -0500

    Verbose pip install

--- install.ps1
+++ install.ps1
@@ -31,5 +31,7 @@ python --version
 python -c "import struct; print(struct.calcsize('P') * 8, 'bits')"
 pip --version
 
-Measure-Command {pip install -r requirements.txt}
+pip install --verbose -r requirements.txt
+
+Measure-Command {pip install --verbose -r requirements.txt}
 pip list
